45960930c816ffec89d650ee299731314e150e41
Milan Jadav <Jadavmilan009@gmail.com>
2020-05-28 15:17:33 +0530

    Update Marksheet.C

--- Marksheet.C
+++ Marksheet.C
@@ -30,7 +30,7 @@ int main()
     printf("Total Marks = %d \n", total);
 
     per = total / 5;
-    printf("Percentage = %d \n\n", per);
+    printf("Percentage = %f \n\n", per);
 
     if (per >= 70)
         printf("Grade = A");
